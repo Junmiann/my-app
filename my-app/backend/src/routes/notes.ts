f3ed150c3d31c94e5c10be3abc034e25c0894424
Junmiann <Mimmixu@gmail.com>
2025-10-02 15:41:49 +0200

    Add post for users
    Added POST-endpoint to add user, added types for user and note

--- my-app/backend/src/routes/notes.ts
+++ my-app/backend/src/routes/notes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
+import type { Note } from "../types/note.js";
 
 const router = Router();
 
-const notes = [
-  { id: 1, userId: 1, text: "Hello /Anna" },
-  { id: 2, userId: 2, text: "This is my first note." },
+const notes: Note[] = [
+  { id: 1, title: "Greeting", writtenByUser: 1, note: "Hello /Anna" },
+  { id: 2, title: "First note", writtenByUser: 2, note: "This is my first note." },
 ];
 
 router.get("/", (req: Request, res: Response) => {
--- my-app/backend/src/routes/users.ts
+++ my-app/backend/src/routes/users.ts
@@ -1,15 +1,27 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
+import type { User } from "../types/user.js";
 
 const router = Router();
 
-const users = [
-  { id: 1, name: "Anna" },
-  { id: 2, name: "Charlie" },
+const users: User[] = [
+  { id: 1, firstName: "Anna", lastName: "Johnson" },
+  { id: 2, firstName: "Bob", lastName: "McKnight" },
 ];
 
 router.get("/", (req: Request, res: Response) => {
     res.json(users);
+});
+
+router.post("/", (req: Request, res: Response) => {
+  const lastUser = users.at(-1);
+  const newId = lastUser ? lastUser.id + 1 : 1;
+
+  const { firstName, lastName } = req.body;
+
+  const newUser = { id: newId, firstName, lastName };
+  users.push(newUser);
+  res.json(newUser);
 });
 
 export default router;
