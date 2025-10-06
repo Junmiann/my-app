134a3baef824b27b5e5d3ddb018efd539f120678
Junmiann <Mimmixu@gmail.com>
2025-10-06 13:00:43 +0200

    Install cors

--- my-app/backend/src/server.ts
+++ my-app/backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import usersRouter from "./routes/users.js";
 import notesRouter from "./routes/notes.js";
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 // Routes
