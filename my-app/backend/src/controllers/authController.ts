0e79d1f511e932732cd0759d3a89c2b66ee71045
Junmiann <Mimmixu@gmail.com>
2025-10-17 11:50:18 +0200

    Add endpoint for registration

--- my-app/backend/src/controllers/authController.ts
+++ my-app/backend/src/controllers/authController.ts
@@ -1,10 +1,24 @@
 import type { Request, Response } from "express";
 import * as authService from "../services/authService.js";
+import type { User } from "../types/user.js";
 
 export const login = async (req: Request, res: Response) => {
     try {
         const user = await authService.authenticateUser(req.body.email, req.body.password);
         res.status(200).json({ user });
+    } catch (error) {
+        res.status(401).json({ error: (error as Error).message });
+    }
+};
+
+export const registration = async (req: Request, res: Response) => {
+    try {
+        const user: User = req.body;
+
+        await authService.validateInputs(user);
+        await authService.createNewUser(user);
+
+        res.status(200).json({ message: "A new user has been created" });
     } catch (error) {
         res.status(401).json({ error: (error as Error).message });
     }
--- my-app/backend/src/services/authService.ts
+++ my-app/backend/src/services/authService.ts
@@ -1,5 +1,6 @@
 import pool from "../db.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import type { User } from "../types/user.js";
 
 export async function authenticateUser(email: string, password: string) {
     try {
@@ -19,5 +20,68 @@ export async function authenticateUser(email: string, password: string) {
 
     } catch (error) {
         throw error;
+    }
+}
+
+export async function createNewUser(user: User) {
+    const client = await pool.connect();
+
+    try {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+
+        await client.query("BEGIN");
+
+        const userResult = await client.query(
+            "INSERT INTO users (first_name, last_name, group_id) VALUES ($1, $2, $3) RETURNING id", 
+            [user.firstName, user.lastName, user.groupId]
+        );
+
+        const userId = userResult.rows[0].id;
+
+        await client.query(
+            "INSERT INTO login_credentials (user_id, email, password) VALUES ($1, $2, $3)", 
+            [userId, user.email, hashedPassword]
+        );
+
+        await client.query("COMMIT");
+
+    } catch(error) {
+        await client.query("ROLLBACK");
+        throw error;
+    } finally {
+        client.release();
+    }
+}
+
+/* Validations */
+export async function validateInputs(user: User): Promise<boolean> {
+    const allFieldsFilled = Object.values(user).every(value => {
+        if (typeof value === "string") {
+            return value.trim() !== "";
+        }
+        return value !== null && value !== undefined;
+    });
+
+    if (!allFieldsFilled) {
+        throw new Error("All fields must be filled in");
+    }
+
+    await validateGroupId(user.groupId);
+    await validateEmail(user.email);
+
+    return true;
+}
+
+async function validateGroupId(groupId: string) {
+    const groupIdResult = await pool.query("SELECT id FROM groups WHERE id = $1", [groupId]);
+    if (groupIdResult.rowCount === 0) {
+      throw new Error("Group ID does not exist");
+    }
+}
+
+async function validateEmail(email: string) {
+    const emailResult = await pool.query("SELECT email FROM login_credentials WHERE email = $1", [email]);
+    if (emailResult.rowCount === 1) {
+      throw new Error("The email is already registered");
     }
 }
